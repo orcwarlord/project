f306df0ab000fd57b9647558597684682f2c9285
Martin Hramiak <orcwarlord@gmail.com>
2023-02-03 11:59:44 +0000

    Initial commit

---
+++ js/fetch.js
@@ -0,0 +1,57 @@
+function loadData(url,callback)
+{	
+	const options = {
+		method: 'GET',
+		mode: 'cors',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	};
+	// avoid cors error
+	
+
+
+	fetch(url, options).then(function(response) {
+		// console.log(response.json);
+		return response.json();
+	}).then(function(json) {
+		callback(json)
+	});
+} //end of loadData()
+
+// loadData(`https://rawg-video-games-database.p.rapidapi.com/games?key=2e5072210c754f0f951b577972c160ba&ids=${searchString}&page_size=40`,populateTopGames);
+
+// function createHandler(plant)
+// {
+// 	//this function uses a closure to associate data with a hyperlink
+// 	return function(){
+// 		//stores the id value into web storage
+// 		sessionStorage.setItem("id",game.id);
+// 	}
+// }
+
+function populateList(plants)
+{
+	console.log(plants.loadData);
+	// const gamesFragment = document.createDocumentFragment();
+	// games.results.forEach(function(game){
+	// 	const newLi=document.createElement("li");
+	// 	const newLink=document.createElement("a");
+	// 	newLink.textContent=game.name;
+	// 	console.log(game.name);
+	// 	//adds a querystring to the URL e.g. details.html?id=2
+	// 	newLink.setAttribute("href","details.html?id="+game.id);
+	// 	newLink.addEventListener("click", createHandler(game), false)
+	// 	newLi.appendChild(newLink);
+	// 	gamesFragment.appendChild(newLi);
+	// })
+	// const gamesList=document.querySelector("#games-list");
+	// gamesList.appendChild(gamesFragment);
+} //end of populateList, do not remove this line
+
+function init(){
+	loadData("http://localhost:3000/data",populateList);
+	
+}
+
+init();
